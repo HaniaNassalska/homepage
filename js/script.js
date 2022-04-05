cd81235a3a33f332f271c2a478ed073d979f829c
Hanna Nassalska <hanna.nassalska@gmail.com>
2022-04-05 11:03:18 +0200

    added background image

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let buttonElement = document.querySelector(".js-header__button");
-let container = document.querySelector(".container");
 let body = document.querySelector(".body");
 let themName = document.querySelector(".themName");
 
 buttonElement.addEventListener("click", () => {
-    container.classList.toggle("container__whiteBackground");
-    body.classList.toggle("body__yellowBackground");
+    body.classList.toggle("body__backgroundImage");
+    body.classList.toggle("body__whiteBackground");
 
-    themName.innerText = body.classList.contains("body__yellowBackground") ? "Przywróć" : "Zmień";
+
+    themName.innerText = body.classList.contains("body__whiteBackground") ? "Przywróć" : "Zmień";
 });
 
